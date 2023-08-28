49d67be2615eed3231d0da08fad8181420c530a0
yifangyujason <996335280@qq.com>
2023-08-28 17:38:57 +0800

    feat: 添加JA3指纹

--- common/env.go
+++ common/env.go
@@ -10,10 +10,6 @@ var (
 	// is debug
 	IS_DEBUG_MODE bool
 	IS_SHOW_USE_U bool
-	// socks
-	SOCKS_URL  string
-	SOCKS_USER string
-	SOCKS_PWD  string
 	// user token
 	USER_TOKEN_ENV_NAME_PREFIX = "Go_Proxy_BingAI_USER_TOKEN"
 	USER_TOKEN_LIST            []string
@@ -32,10 +28,6 @@ func initEnv() {
 	// is debug
 	IS_DEBUG_MODE = os.Getenv("Go_Proxy_BingAI_Debug") != ""
 	IS_SHOW_USE_U = os.Getenv("Go_Proxy_BingAI_SHOW_U") != ""
-	// socks
-	SOCKS_URL = os.Getenv("Go_Proxy_BingAI_SOCKS_URL")
-	SOCKS_USER = os.Getenv("Go_Proxy_BingAI_SOCKS_USER")
-	SOCKS_PWD = os.Getenv("Go_Proxy_BingAI_SOCKS_PWD")
 	// auth
 	AUTH_KEY = os.Getenv("Go_Proxy_BingAI_AUTH_KEY")
 	BingAI_TOKEN_URL = os.Getenv("Go_Proxy_BingAI_TOKEN_URL")
